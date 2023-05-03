3820baed633470d1ba6eab562c8564f092cfd42a
ajaymsd <ajbalasubramanian2002@gmail.com>
2023-05-03 13:26:52 +0530

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config;
 const mongooseConnect=require("./config/db");
 const authRoutes=require('./routes/blog');
 const cors=require('cors');
+const PORT_NO=process.env.PORT || 6010 
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,6 @@ app.use(express.static("public/upload"));
 
 app.use("/api/v1",authRoutes);
 mongooseConnect();
-app.listen(process.env.PORT,(req,res)=>{
+app.listen(PORT_NO,(req,res)=>{
     console.log(`App Listening on http://localhost:${process.env.PORT}`);
 })
